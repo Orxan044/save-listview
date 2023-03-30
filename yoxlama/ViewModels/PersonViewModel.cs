a190e9973130c11dad178424236788124d9c4d58
Husey_so01 <Husey_so01@STHQ012E-04>
2023-03-30 11:35:59 +0400

    Add project files.

---
+++ yoxlama/ViewModels/PersonViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using yoxlama.Models;
+
+namespace yoxlama.ViewModels
+{
+    public class PersonViewModel : INotifyPropertyChanged
+    {
+
+        private Person _person;
+
+        public Person Person
+        {
+            get { return _person; }
+            set { _person = value;
+                OnPropertyChanged(nameof(Person));  
+            }
+        }
+
+        public PersonViewModel() => Person = new Person();
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+
+    }
+}
